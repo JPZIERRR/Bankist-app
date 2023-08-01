bb841e91881539814f4b49eebdfd12b2a6f08dce
JPZIERRR <joaofornaz_ier@hotmail.com>
2023-08-01 17:27:34 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const port = 3003;
+
+const path = require('path');
+
+const pathName = path.join(__dirname, 'templates');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(`${pathName}/index.html`);
+});
+
+app.listen(port, () => {
+  console.log('server rodando');
+});
